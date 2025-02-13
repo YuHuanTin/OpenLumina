b963f257d3c0ec7d7e976ab9564d77f44284d98c
AFETT <git@git.com>
2025-02-13 22:39:26 +0800

    + CMake OpenLumina2

---
+++ OpenLumina2/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// std
+#include <fstream>
+#include <print>
+
+// 3-rd party
+#include <MinHook.h>
+
+// ida
+#include <ida.hpp>
+#include <diskio.hpp>
+#include <idp.hpp>
+#include <kernwin.hpp>
+#include <loader.hpp>
+
+// windows
+#include "Windows.h"
+#include "wincrypt.h"
